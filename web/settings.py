db1b66cbba1632ccbe44ff8edeb58034c2e2024c
Swapnil Chandra <63166850+swapnil-dot@users.noreply.github.com>
2021-10-05 02:09:58 -0400

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3$!e5*ggej1g%8n!d77u^5jk$qy&_p_6r_gc2+mdu(2v1-&c%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
